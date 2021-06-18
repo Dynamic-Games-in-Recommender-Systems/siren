c75d1ceefa36297aa1ec16f740b3529973d360bc
TimHeckmans <t.heckmans@student.maastrichtuniversity.nl>
2021-06-18 10:37:34 +0200

    Revert "Restoring experiments"
    This reverts commit e691adb1a335afbf66e9801994bbecda0ebe4ccc.

--- experiments.py
+++ experiments.py
@@ -8,34 +8,70 @@ b = 2
 c = 2
 num_particles = 8
 num_generations = 20
-pi = [
-    1.8,
-    1.2,
-    1.2,
-    1.2,
-    1.2,
-    1.05,
-    1.05,
-    1.05,
-    1.05,
-    1.05,
-    1.05,
-    1.05,
-    1.05
-]
+# pi = [
+#     1.8,
+#     1.2,
+#     1.2,
+#     1.2,
+#     1.2,
+#     1.05,
+#     1.05,
+#     1.05,
+#     1.05,
+#     1.05,
+#     1.05,
+#     1.05,
+#     1.05
+# ]
+
+def experiment_a():
+    # a_arr = [1 + (i*.2) for i in range(16)]
+    print("START EXPERIMENT")
+    a_arr = [1, 2, 3]
+    met_arr = pd.DataFrame()
+
+    for a in a_arr:
+        sim = Simulation()
+        sim.setSettings()
+        sim.initWithSettings()
+        sim.runSimulation(a, b, c, pi, num_particles, num_generations)
+        met_arr = met_arr.append(sim.met, ignore_index=True)
+
+    print(met_arr)
+
+    ## Return the metrics and maybe store in csv file.
+
+def experiment_b():
+    b_arr = [i*0.4 for i in range(21)]
+    print("START EXPERIMENT")
+    b_arr = [0, 1, 2]
+    met_arr = pd.DataFrame()
+
+    for b in b_arr:
+        sim = Simulation()
+        sim.setSettings()
+        sim.initWithSettings()
+        sim.runSimulation(a, b, c, pi, num_particles, num_generations)
+        met_arr = met_arr.append(sim.met_out, ignore_index=True)
+
+    print(met_arr)
+
+def experiment_c():
+    pass
 
 def experiment_pi():
-    pi_arr = [[1.8, 1.2, 1.2, 1.2, 1.2, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05],
-              [2, 1.1, 1.1, 1.1, 1.1, 1.01, 1.01, 1.01, 1.01, 1.01, 1.01, 1.01, 1.01],
-              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-    # pi_arr = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # pi_arr = [[1.8, 1.2, 1.2, 1.2, 1.2, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05],
     #           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    #           [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]]
+    #           [2, 1.1, 1.1, 1.1, 1.1, 1.01, 1.01, 1.01, 1.01, 1.01, 1.01, 1.01, 1.01]]
+    pi_arr = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+              [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]]
     print("START EXPERIMENT")
     met_arr = pd.DataFrame()
+    comp_arr = pd.DataFrame()
     day_arr = np.array([])
     exp_arr = []
-    time_arr = []
+    arr = []
     first_run = True
 
     for ii, pi in enumerate(pi_arr):
@@ -48,68 +84,41 @@ def experiment_pi():
             sim.setSettings()
             sim.initWithSettings()
             sim.runSimulation(a, b, c, pi, num_particles, num_generations, game_trigger=False)
-            met_arr = met_arr.append(sim.met_out, ignore_index=True)
-            time_arr = time_arr + sim.time
-            exp_arr = exp_arr + (['Base'] * 10)
+            for i in range(9):
+                arr.append(sim.met_out[i]['EPC'])
+            print(arr)
+            comp_arr['Base'] = arr
 
+        arr = []
         sim = Simulation()
         sim.setSettings()
         sim.initWithSettings()
         sim.runSimulation(a, b, c, pi, num_particles, num_generations, game_trigger=True)
         met_arr = met_arr.append(sim.met_out, ignore_index=True)
-        day_arr = np.append(day_arr, np.arange(1, 11))
-        exp_arr = exp_arr + (['Pi' + str(ii + 1)] * 10)
+        day_arr = np.append(day_arr, np.arange(1, 10))
+        exp_arr = exp_arr + ([ii + 1] * 9)
+        for i in range(9):
+            arr.append(sim.met_out[i]['EPC'])
+        print(arr)
+        comp_arr['Pi' + str(ii + 1)] = arr
+        print(comp_arr)
 
-    met_arr['Time'] = time_arr
-    met_arr['Day'] = day_arr
-    met_arr['Exp'] = exp_arr
+    met_arr.insert(0, 'day', day_arr)
+    met_arr.insert(0, 'exp', exp_arr)
     print(met_arr)
-
+    print(comp_arr)
 
 def experiment_particles():
-    particle_arr = [2, 10, 20]
-    print("START EXPERIMENT")
-    met_arr = pd.DataFrame()
-    day_arr = np.array([])
-    exp_arr = []
-    time_arr = []
-    first_run = True
+    pass
 
-    for ii, num_particles in enumerate(particle_arr):
-        print("Testing num_particles = ", num_particles)
-
-        # Run base case
-        if first_run:
-            first_run = False
-            sim = Simulation()
-            sim.setSettings()
-            sim.initWithSettings()
-            sim.runSimulation(a, b, c, pi, num_particles, num_generations, game_trigger=False)
-            met_arr = met_arr.append(sim.met_out, ignore_index=True)
-            time_arr = time_arr + sim.time
-            exp_arr = exp_arr + (['Base'] * 10)
-
-        sim = Simulation()
-        sim.setSettings()
-        sim.initWithSettings()
-        sim.runSimulation(a, b, c, pi, num_particles, num_generations, game_trigger=True)
-        met_arr = met_arr.append(sim.met_out, ignore_index=True)
-        day_arr = np.append(day_arr, np.arange(1, 11))
-        exp_arr = exp_arr + ([str(num_particles)] * 10)
-
-    met_arr['Time'] = time_arr
-    met_arr['Day'] = day_arr
-    met_arr['Exp'] = exp_arr
-    print(met_arr)
-    met_arr.to_csv('PI experiments.csv')
-
-# This experiment is different <- needs all EPC values for each generation for each day
 def experiment_generations():
-    num_generations = 200
+    pass
 
 def experiment_users():
-    use_arr = [5, 10, 20]
+    pass
 
 def experiment_articles():
-    articles_arr = [5, 13, 20]
+    pass
 
+
+experiment_pi()
