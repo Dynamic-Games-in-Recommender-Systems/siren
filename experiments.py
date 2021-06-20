56cc6c2141f5a05434ae7a579a060a76ad608d58
chrisdt1998 <chris.dutoit98@gmail.com>
2021-06-20 12:08:08 +0200

    Experiment pi

--- experiments.py
+++ experiments.py
@@ -3,62 +3,28 @@ import pandas as pd
 import numpy as np
 import time
 pd.set_option("display.max_rows", None, "display.max_columns", None)
+from tqdm import tqdm
 
 a = 2
 b = 2
 c = 2
-num_particles = 8
-num_generations = 5
-# pi = [
-#     1.8,
-#     1.2,
-#     1.2,
-#     1.2,
-#     1.2,
-#     1.05,
-#     1.05,
-#     1.05,
-#     1.05,
-#     1.05,
-#     1.05,
-#     1.05,
-#     1.05
-# ]
-
-def experiment_a():
-    # a_arr = [1 + (i*.2) for i in range(16)]
-    print("START EXPERIMENT")
-    a_arr = [1, 2, 3]
-    met_arr = pd.DataFrame()
-
-    for a in a_arr:
-        sim = Simulation()
-        sim.setSettings()
-        sim.initWithSettings()
-        sim.runSimulation(a, b, c, pi, num_particles, num_generations)
-        met_arr = met_arr.append(sim.met, ignore_index=True)
-
-    print(met_arr)
-
-    ## Return the metrics and maybe store in csv file.
-
-def experiment_b():
-    b_arr = [i*0.4 for i in range(21)]
-    print("START EXPERIMENT")
-    b_arr = [0, 1, 2]
-    met_arr = pd.DataFrame()
-
-    for b in b_arr:
-        sim = Simulation()
-        sim.setSettings()
-        sim.initWithSettings()
-        sim.runSimulation(a, b, c, pi, num_particles, num_generations)
-        met_arr = met_arr.append(sim.met_out, ignore_index=True)
-
-    print(met_arr)
-
-def experiment_c():
-    pass
+num_particles = 16
+num_generations = 200
+pi = [
+    1.8,
+    1.2,
+    1.2,
+    1.2,
+    1.2,
+    1.05,
+    1.05,
+    1.05,
+    1.05,
+    1.05,
+    1.05,
+    1.05,
+    1.05
+]
 
 def experiment_pi():
     pi_arr = [[1.8, 1.2, 1.2, 1.2, 1.2, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05],
@@ -73,34 +39,37 @@ def experiment_pi():
     day_arr = np.array([])
     exp_arr = []
     time_arr = []
-    first_run = True
 
-    for ii, pi in enumerate(pi_arr):
-        print("Testing pi = ", pi)
 
-        # Run base case
-        if first_run:
-            first_run = False
+    for _ in tqdm(range(3)):
+        first_run = True
+
+        for ii, pi in enumerate(pi_arr):
+            print("Testing pi = ", pi)
+
+            # Run base case
+            if first_run:
+                first_run = False
+                sim = Simulation()
+                sim.setSettings()
+                sim.initWithSettings()
+                sim.runSimulation(a, b, c, pi, num_particles, num_generations, game_trigger=False)
+
+                met_arr = met_arr.append(sim.met_out, ignore_index=True)
+                time_arr = time_arr + sim.time
+                day_arr = np.append(day_arr, np.arange(1, 11))
+                exp_arr = exp_arr + (['Base'] * 10)
+
+
             sim = Simulation()
             sim.setSettings()
             sim.initWithSettings()
-            sim.runSimulation(a, b, c, pi, num_particles, num_generations, game_trigger=False)
+            sim.runSimulation(a, b, c, pi, num_particles, num_generations, game_trigger=True)
 
             met_arr = met_arr.append(sim.met_out, ignore_index=True)
             time_arr = time_arr + sim.time
             day_arr = np.append(day_arr, np.arange(1, 11))
-            exp_arr = exp_arr + (['Base'] * 10)
-
-
-        sim = Simulation()
-        sim.setSettings()
-        sim.initWithSettings()
-        sim.runSimulation(a, b, c, pi, num_particles, num_generations, game_trigger=True)
-
-        met_arr = met_arr.append(sim.met_out, ignore_index=True)
-        time_arr = time_arr + sim.time
-        day_arr = np.append(day_arr, np.arange(1, 11))
-        exp_arr = exp_arr + (['Pi' + str(ii + 1)] * 10)
+            exp_arr = exp_arr + (['Pi' + str(ii + 1)] * 10)
 
     #
     # print(len(time_arr))
@@ -113,17 +82,6 @@ def experiment_pi():
     met_arr.to_excel('Results\pi-experiment-' + str(time.time())+ '.xlsx')
     print(met_arr)
 
-def experiment_particles():
-    pass
-
-def experiment_generations():
-    pass
-
-def experiment_users():
-    pass
-
-def experiment_articles():
-    pass
 
 
 experiment_pi()
